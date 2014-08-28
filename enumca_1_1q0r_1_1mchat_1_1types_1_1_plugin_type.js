576c06f2ee80daf2f40d96807c1d02ccafb96ad5
MiracleM4n <MiracleM4n@gmail.com>
2014-08-28 03:45:07 +0000

    [Refactor] Make Vault calls less ambiguous. Separate Vault into Vault Permissions / Vault Chat.

--- enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.js
+++ enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.js
@@ -3,6 +3,6 @@ var enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type =
     [ "LEVELED_NODES", "enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#a732085ebda23500d26e46d4ad27ac4cd", null ],
     [ "NEW_INFO", "enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#abf32d812a4c7cbb056b88dbf479af234", null ],
     [ "OLD_NODES", "enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#a8fe75ce524a1b2682682cc049bfa96ef", null ],
-    [ "VAULT", "enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#af4ead02898fb91d17f3e45c1e02aab79", null ],
-    [ "VAULT_CHAT", "enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#ad1e92828e8e5207a7e56b496d55a299c", null ]
+    [ "VAULT_CHAT", "enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#ad1e92828e8e5207a7e56b496d55a299c", null ],
+    [ "VAULT_PERM", "enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#a4dc54bead85868c7948cb23d8da7c9bb", null ]
 ];
--- search/all_14.js
+++ search/all_14.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['var',['Var',['../classca_1_1q0r_1_1mchat_1_1variables_1_1_var.html',1,'ca::q0r::mchat::variables']]],
   ['variablemanager',['VariableManager',['../classca_1_1q0r_1_1mchat_1_1variables_1_1_variable_manager.html',1,'ca::q0r::mchat::variables']]],
-  ['vault',['VAULT',['../enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#af4ead02898fb91d17f3e45c1e02aab79',1,'ca::q0r::mchat::types::PluginType']]],
-  ['vault_5fchat',['VAULT_CHAT',['../enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#ad1e92828e8e5207a7e56b496d55a299c',1,'ca::q0r::mchat::types::PluginType']]]
+  ['vault_5fchat',['VAULT_CHAT',['../enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#ad1e92828e8e5207a7e56b496d55a299c',1,'ca::q0r::mchat::types::PluginType']]],
+  ['vault_5fperm',['VAULT_PERM',['../enumca_1_1q0r_1_1mchat_1_1types_1_1_plugin_type.html#a4dc54bead85868c7948cb23d8da7c9bb',1,'ca::q0r::mchat::types::PluginType']]]
 ];
